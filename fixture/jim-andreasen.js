2d41ca59d13c50f63fa608a3f46c0a5bd364b6a5
Jim Andreasen <reifnir@gmail.com>
2012-11-19 00:20:46 -0500

    added language usage by job

--- fixture/jim-andreasen.js
+++ fixture/jim-andreasen.js
@@ -293,7 +293,12 @@
             "StartDate": "2010-07-01T00:00:00",
             "Title": "Sr. Software Engineer",
             "Address": "King of Prussia, PA",
-            "Description": "Individually completed a partially implemented and abandoned-as-impossible attempt at rearchitecting their core application leveraging Dependency Injection and Repository patterns. Was a principle leader in shaping and implementing the API/backend design of the core application's more-UX focused reboot as a RESTful, more unit-testable API. Provided mentoring and assistance for other developers in algorithms, troubleshooting and design. Was a leading contributer to the new rule-based dynamic content service's early development project. Received the peer nominated, management approved Roller of the year award in 2011. The majority of development was done in C# 4.0 in ASP.NET MVC3 (TDD, Repo pattern, IoC => DI, MVC3 as a JSON REST API), WCF (service per-request, custom handlers and behavior, SOAP, WebApi and CodePlex WCF Web API), and JavaScript (closures, prototypical inheritance and JSMVC)."
+            "Description": "Individually completed a partially implemented and abandoned-as-impossible attempt at rearchitecting their core application leveraging Dependency Injection and Repository patterns. Was a principle leader in shaping and implementing the API/backend design of the core application's more-UX focused reboot as a RESTful, more unit-testable API. Provided mentoring and assistance for other developers in algorithms, troubleshooting and design. Was a leading contributer to the new rule-based dynamic content service's early development project. Received the peer nominated, management approved Roller of the year award in 2011. The majority of development was done in C# 4.0 in ASP.NET MVC3 (TDD, Repo pattern, IoC => DI, MVC3 as a JSON REST API), WCF (service per-request, custom handlers and behavior, SOAP, WebApi and CodePlex WCF Web API), and JavaScript (closures, prototypical inheritance and JSMVC).",
+            "LanguageConcentration": [
+                { "Name": "C#", "Weight": 0.7 },
+                { "Name": "SQL", "Weight": 0.25 },
+                { "Name": "JavaScript", "Weight": 0.05 }
+            ]
         },
         {
             "CompanyName": "Allied Barton Security Services",
@@ -301,7 +306,13 @@
             "EndDate": "2010-06-01T00:00:00",
             "Title": "Sr. Developer",
             "Address": "Conshohocken, PA",
-            "Description": "Primarily responsible for supplanting a broken, internal benefit management system for the company's 58k employees assigned to over 200 distinct plans. The majority of development was done in C# 3.5 class libraries and an ASP.NET front end and T-SQL. XSLT, SSIS, SSRS, VB6, and VB.NET 1.1 were also used to a much lesser extent. Other projects included: EDI, an Active Directory class library, a DataTable to Excel FileStream web service and XHTML and CSS W3C compliance."
+            "Description": "Primarily responsible for supplanting a broken, internal benefit management system for the company's 58k employees assigned to over 200 distinct plans. The majority of development was done in C# 3.5 class libraries and an ASP.NET front end and T-SQL. XSLT, SSIS, SSRS, VB6, and VB.NET 1.1 were also used to a much lesser extent. Other projects included: EDI, an Active Directory class library, a DataTable to Excel FileStream web service and XHTML and CSS W3C compliance.",
+            "LanguageConcentration": [
+                { "Name": "SQL", "Weight": 0.65 },
+                { "Name": "C#", "Weight": 0.3 },
+                { "Name": "VB6", "Weight": 0.03 },
+                { "Name": "VB.NET", "Weight": 0.02 }
+            ]
         },
         {
             "CompanyName": "MedRisk, Inc.",
@@ -309,7 +320,13 @@
             "EndDate": "2008-03-01T00:00:00",
             "Title": "Sr. Developer",
             "Address": "King of Prussia, PA",
-            "Description": "Programming focus was object oriented and database design and development. Programming predominantly done in C#, T-SQL, VB6 and Access VBA. Projects include creation of COM Objects/DLLs, Web Services, SSRS web service integration, automation applications, MSSQL Procedures, index optimization, data manipulation, automated email/attachment to image conversion service, and creation of rules based routing engines. In addition to programming, project design, mentoring new programmers and requirement gathering were necessary from all levels within the organization. Five years of experience attaining healthcare industry and medical billing business knowledge."
+            "Description": "Programming focus was object oriented and database design and development. Programming predominantly done in C#, T-SQL, VB6 and Access VBA. Projects include creation of COM Objects/DLLs, Web Services, SSRS web service integration, automation applications, MSSQL Procedures, index optimization, data manipulation, automated email/attachment to image conversion service, and creation of rules based routing engines. In addition to programming, project design, mentoring new programmers and requirement gathering were necessary from all levels within the organization. Five years of experience attaining healthcare industry and medical billing business knowledge.",
+            "LanguageConcentration": [
+                { "Name": "C#", "Weight": 0.60 },
+                { "Name": "SQL", "Weight": 0.30 },
+                { "Name": "VBA", "Weight": 0.07 },
+                { "Name": "VB6", "Weight": 0.03 }
+            ]
         },
         {
             "CompanyName": "Automated Financial Solutions",
@@ -317,7 +334,13 @@
             "EndDate": "2006-10-01T00:00:00",
             "Title": "Programmer Analyst",
             "Address": "Exton, PA",
-            "Description": "Programming focus was on object oriented distributed processing in an n-tier architecture in VB6, T-SQL, and XML for loan booking front end. Projects included: creation of COM Objects/DLLs, subclassing a web browser control for kiosk-like control over web functionality and event processing. Database storage and integration of PDF files as a means of document management, designed database archival process with on-demand retrieval processing. Business knowledge attained in loan booking aspects of the banking industry."
+            "Description": "Programming focus was on object oriented distributed processing in an n-tier architecture in VB6, T-SQL, and XML for loan booking front end. Projects included: creation of COM Objects/DLLs, subclassing a web browser control for kiosk-like control over web functionality and event processing. Database storage and integration of PDF files as a means of document management, designed database archival process with on-demand retrieval processing. Business knowledge attained in loan booking aspects of the banking industry.",
+            "LanguageConcentration": [
+                { "Name": "VB6", "Weight": 0.8 },
+                { "Name": "SQL", "Weight": 0.1 },
+                { "Name": "XML", "Weight": 0.05 },
+                { "Name": "VBScript", "Weight": 0.05 }
+            ]
         },
         {
             "CompanyName": "MedRisk, Inc.",
@@ -325,7 +348,12 @@
             "EndDate": "2005-12-01T00:00:00",
             "Title": "Sr. Developer",
             "Address": "King of Prussia, PA",
-            "Description": "[See description above]"
+            "Description": "[See description above]",
+            "LanguageConcentration": [
+                { "Name": "VBA", "Weight": 0.5 },
+                { "Name": "SQL", "Weight": 0.45 },
+                { "Name": "VB6", "Weight": 0.05 }
+            ]
         },
         {
             "CompanyName": "Elecor Communications",
