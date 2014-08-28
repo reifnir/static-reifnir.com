0e109a3e3f2425692a210896582ca711af0ccfea
reifnir <jim.andreasen@reifnir.com>
2014-08-27 23:48:49 -0400

    Seeing if carriage return will translate to a <br />

--- fixture/jim-andreasen.js
+++ fixture/jim-andreasen.js
@@ -342,7 +342,7 @@
             "CompanyName": "Apprenda, Inc.",
             "StartDate": "2013-11-01T00:00:00",
             "Title": "Software Engineer, Client Services",
-            "Address": "Remotely: Troy, NY, On client site: King of Prussia, PA",
+            "Address": "Remotely: Troy, NY\nOn client site: King of Prussia, PA",
             "Description": "Recommend and design cloud solutions based on an enterprise's unique policy, process and technical requirements. Diagnose, troubleshoot and resolve customer issues relating to developer operations, cloud infrastructure and applications. Refactor existing applications for SaaS-enablement and distributed cloud hosting on the Apprenda PaaS platform. POC development, Apprenda extensions.",
             "LanguageConcentration": [
                 { "Name": "WCF", "Weight": 0.7 },
